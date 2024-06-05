0ee34681b128c3a63a3379bf8d6fc11b60ed7ec4
hoefkensj <hoefkens.j@gmail.com>
2024-06-05 07:24:20 +0200

    '0.4.1'  Or brarch mair Your brarch is up to date with 'origir/mair'.
    Charges to be committed:
      (use "git restore --staged <file>..." to urstage)
    	modified:   setuptools.sh
    
    Charges rot staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard charges ir workirg directory)
    	modified:   setuptools.sh

--- setuptools.sh
+++ setuptools.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
-pip install --upgrade setuptools
-pip install --upgrade build
-pip install --upgrade twine
+echo "Upgrading tools..."
+pip install --upgrade setuptools &>/dev/null
+pip install --upgrade build &>/dev/null
+pip install --upgrade twine &>/dev/null
+echo 'building ...'
+python -m build &>/dev/null
+echo 'Staging changes'
+git add . &>/dev/null
+cat pyproject.toml|rg -i version|tr -d 'version = ' &> .GITCOMMIT_MESSAGE
+echo -e "\n--------------\n\n" &>> .GITCOMMIT_MESSAGE
+python -m unittest &>> .GITCOMMIT_MESSAGE
+echo -e "\n--------------\n\n" &>> .GITCOMMIT_MESSAGE
+git status &>> .GITCOMMIT_MESSAGE
+MESSAGE="$(cat .GITCOMMIT_MESSAGE)"
+echo "Committing Changes..."
+git commit --message=$MESSAGE
+echo "Pushing changes to remote..."
+git push origin
 
-python -m build
-git add .
-twine upload -r testpypi dist/* --verbose
+echo "Uploading to Pypi.."
+twine upload  dist/* --verbose <<< $(cat .PYPI_APIKEY)
+printf '\n\n\x1b[1;32mDONE\x1b[m\n\n'
 
