d30468ab50218f1460d9c827174077be9eba575e
hoefkensj <hoefkens.j@gmail.com>
2024-06-05 08:56:02 +0200

    CURRENT VERSION: '0.4.1' :: TESTS: OK :: CHANGED:

--- setuptools.sh
+++ setuptools.sh
@@ -3,24 +3,24 @@ C1A=1
 C1B=30
 C2A=41
 C2B=70
-DONE='\xb[1;32mDONE\x1b[m'
+
 function ptab(){
-	printf '\x1b[%sG%s' $1 $2
+	printf '\x1b[%sG\x1b[%sm%s\x1b[m' $1 $2 $3
 }
 VERSION=$(cat pyproject.toml|rg -i version|tr -d 'version = ')
 PROJ=$(basename $PWD)
-ptab $C1A "Project:"
-ptab $C1B $PROJ
-ptab $C2A "Version:"
-ptab $C2B $VERSION
+ptab $C1A 29 "Project:"
+ptab $C1B 32 $PROJ
+ptab $C2A 29 "Version:"
+ptab $C2B 32 $VERSION
 printf '\n'
-ptab $C1A "Upgrading tools:"
+ptab $C1A 29 "Upgrading tools:"
 pip install --upgrade setuptools &>/dev/null
 pip install --upgrade build &>/dev/null
 pip install --upgrade twine &>/dev/null
-ptab $C1B $DONE
+ptab $C1B 32 "DONE"
 printf '\n'
-ptab $C1A'Running Tests:'
+ptab $C1A 28 'Running Tests:'
 python -m unittest &> .STATUS_TESTS
 [[ -n $(cat .STATUS_TESTS|rg -i '^OK$') ]] && TESTSTATUS='OK' || TESTSTATUS='FAIL'
 rm .STATUS_TESTS
