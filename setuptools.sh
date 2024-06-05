37fd019b55e24772ded5a39b2d36b63766d55d37
hoefkensj <hoefkens.j@gmail.com>
2024-06-05 08:34:26 +0200

    CURRENT VERSION: '0.4.1' :: TESTS: OK :: CHANGED:

--- setuptools.sh
+++ setuptools.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+C1A="\x1b[1G"
+C1B='\x1b[30G'
+C2A='\x1b[41G'
+C2B='\x1b[70G'
+alias ptab="printf '%s%s:%s\x1b[32m%s\x1b[m\n'"
 VERSION=$(cat pyproject.toml|rg -i version|tr -d 'version = ')
 PROJ=$(basename $PWD)
-printf "PROJECT:\x1b[30G%s\x1b[40GVERSION:\x1b[70G%s\n===========================================================================\n" $PROJ $VERSION
+ptab $C1A "Project" $C1B $PROJ
+ptab $C2A "Version" $C2B $VERSION
+
+
 printf "Upgrading tools..."
 pip install --upgrade setuptools &>/dev/null
 pip install --upgrade build &>/dev/null
@@ -15,17 +23,18 @@ printf '\t\t\x1b[32mDONE\x1b[m\t\tRESULT: \x1b[32m%s\x1b[m\n' $TESTSTATUS
 printf 'building %s v%s ...' $PROJ $VERSION
 python -m build &>/dev/null
 printf '\t\t\x1b[32mDONE\x1b[m\n'
-printf 'GIT:\n----------------------------------------------------------------------\n\t- Staging:\x1b[30G '
+printf 'GIT:'
+printf '\x1b[41GStaging:'
 git add . &>/dev/null
-printf ' \x1b[32mDONE\x1b[m\n'
-printf '\t- Committing:\x1b[30G'
+printf '\x1b[70G\x1b[32mDONE\x1b[m\n'
+printf '\tCommitting:'
 echo "CURRENT VERSION: $VERSION :: TESTS: $TESTSTATUS :: CHANGED: " > .GITCOMMIT_MESSAGE
 git status &>> .GITCOMMIT_MESSAGE &>/dev/null
 git commit -m "$(cat .GITCOMMIT_MESSAGE)" &>/dev/null
-printf '\x1b[32mDONE\x1b[m\n'
-printf '\t- Pushing:\x1b[30G'
+printf '\x1b[30G\x1b[32mDONE\x1b[m\n'
+printf '\tPushing'
 git push &>/dev/null
-printf '\x1b[32mDONE\x1b[m\n'
+printf '\x1b[30G\x1b[32mDONE\x1b[m\n'
 printf '\n----------------------------------------------------------------------\n'
 printf "Uploading to Pypi..\x1b[30G"
 #twine upload  dist/* --verbose  --skip-existing  -u '__token__' -p "$(cat .PYPI_APIKEY)" &>/dev/null
